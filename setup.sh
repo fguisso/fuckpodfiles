509852d980e58e7eeb6645f7fc0b1144da12f938
Fernando Guisso <fguisso@gmail.com>
2022-09-15 00:16:12 -0300

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+# Clone original fuckfiles
+git clone https://github.com/fguisso/fuckfiles ~/
+
+
+# Install aws cli
+if ! command -v aws &> /dev/null; then
+
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+unzip awscliv2.zip
+sudo ./aws/install
+
+fi
+
+# Install terraform
+if ! command -v /usr/bin/terraform &> /dev/null; then
+
+sudo apt-get update && sudo apt-get install -y gnupg software-properties-common curl
+curl -fsSL https://apt.releases.hashicorp.com/gpg | sudo apt-key add -
+sudo apt-add-repository "deb [arch=amd64] https://apt.releases.hashicorp.com $(lsb_release -cs) main"
+sudo apt-get update && sudo apt-get install terraform
+
+fi
+
+# Install gcloud
+if ! command -v gcloud &> /dev/null; then
+
+sudo apt-get install apt-transport-https ca-certificates gnupg --yes
+echo "deb [signed-by=/usr/share/keyrings/cloud.google.gpg] https://packages.cloud.google.com/apt cloud-sdk main" | sudo tee -a /etc/apt/sources.list.d/google-cloud-sdk.list
+curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key --keyring /usr/share/keyrings/cloud.google.gpg add -
+sudo apt-get update && sudo apt-get install google-cloud-cli
+
+fi
