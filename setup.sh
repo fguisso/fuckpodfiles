eb8138f56dd162755389831c3fa017337698fea5
Fernando Guisso <fguisso@gmail.com>
2022-09-15 00:26:58 -0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 
 # Clone original fuckfiles
-git clone https://github.com/fguisso/fuckfiles ~/
+git clone https://github.com/fguisso/fuckfiles
+
+cd fuckfiles
+
+ln -s .p10k ~/.p10k
+ln -s .zshrc ~/.zshrc
+ln -s .vimrc ~/.vimrc
+ln -s .gitconfig ~/.gitconfig
